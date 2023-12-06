a8f63210e748c9640a7835565fc2eca1f4fa7f37
Ben Hancock <benjamin@hancock1.com>
2023-12-05 20:16:44 -0700

    updated index.js in the function folder

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,13 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-
-app.use(express.json());
-app.use(cors);
-
-app.get("/", (req, res) => {
-  let message = "Yay you got a response!";
-
-  res.status(200).send(message);
-});
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello World!",
+    }),
+  };
+};
